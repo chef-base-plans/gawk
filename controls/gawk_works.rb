39eec6f1d9ab9ed8e62ca7e949adc854abdac4fc
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-08 16:44:17 +0100

    Add descriptions
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/gawk_works.rb
+++ controls/gawk_works.rb
@@ -2,20 +2,23 @@ title 'Tests to confirm gawk works as expected'
 
 plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'gawk')
+plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+plan_pkg_ident = ((plan_installation_directory.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
+plan_pkg_version = (plan_pkg_ident.match /^#{plan_origin}\/#{plan_name}\/(?<version>.*)\//)[:version]
 
 control 'core-plans-gawk-works' do
   impact 1.0
   title 'Ensure gawk works as expected'
   desc '
+  Verify gawk by ensuring (1) its installation directory exists and (2) that
+  it returns the expected version
   '
-  plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+  
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
   end
   
-  plan_pkg_ident = ((plan_installation_directory.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
-  plan_pkg_version = (plan_pkg_ident.match /^#{plan_origin}\/#{plan_name}\/(?<version>.*)\//)[:version]
   describe command("DEBUG=true; hab pkg exec #{plan_pkg_ident} gawk --version") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
