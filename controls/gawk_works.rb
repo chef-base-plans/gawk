9a0e0a5dc7a945e98ae5297ae9d381ee3d564601
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-16 17:05:50 +0100

    Remove DEBUG and update according to pattern
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/gawk_works.rb
+++ controls/gawk_works.rb
@@ -7,19 +7,22 @@ control 'core-plans-gawk-works' do
   impact 1.0
   title 'Ensure gawk works as expected'
   desc '
-  Verify gawk by ensuring (1) its installation directory exists and (2) that
-  it returns the expected version
+  Verify gawk by ensuring 
+  (1) its installation directory exists and 
+  (2) that it returns the expected version.
   '
   
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
   end
   
-  plan_pkg_ident = ((plan_installation_directory.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
-  plan_pkg_version = (plan_pkg_ident.match /^#{plan_origin}\/#{plan_name}\/(?<version>.*)\//)[:version]
-  describe command("DEBUG=true; hab pkg exec #{plan_pkg_ident} gawk --version") do
+  command_relative_path = input('command_relative_path', value: 'bin/gawk')
+  command_full_path = File.join(plan_installation_directory.stdout.strip, "#{command_relative_path}")
+  plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
+  describe command("#{command_full_path} --version") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
     its('stdout') { should match /GNU Awk #{plan_pkg_version}/ }
